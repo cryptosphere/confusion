abbb43750715b40925e69fe3fdd61bda431fed74
Tony Arcieri <bascule@gmail.com>
2013-10-30 14:48:43 -0700

    Indentation fix

--- lib/confusion/resource.rb
+++ lib/confusion/resource.rb
@@ -2,10 +2,10 @@ require 'lattice'
 
 module Confusion
   class Resource < Lattice::Resource
-  	def finish_request
+    def finish_request
       response.headers["Content-Security-Policy"] = "default-src 'self'"
       response.headers["X-Content-Type-Options"]  = "nosniff"
       response.headers["X-Frame-Options"]         = "DENY"
-  	end
+    end
   end
 end
