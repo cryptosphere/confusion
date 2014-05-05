10973cb67c8a10376fd0216f3cd34d02b6462b81
Tony Arcieri <bascule@gmail.com>
2014-05-05 02:22:55 -0700

    Pulse red on incorrect password

--- webui/js/password.js
+++ webui/js/password.js
@@ -9,4 +9,31 @@ function openSesame() {
   return false;
 }
 
-$("#password-form").submit(openSesame);
+function passwordIncorrect() {
+  var overlay = $("#password-sphere-overlay");
+  overlay.fadeTo(0, 1);
+  overlay.show();
+  overlay.fadeTo(1000, 0);
+}
+
+function login() {
+  var password = $("#password-field").val();
+
+  $.ajax({
+    type: "POST",
+    url: "/login/",
+    data: $("password-form").serialize(),
+    async: true,
+    success: function(response, status, xhr) {
+      alert("success");
+      openSesame();
+    },
+    error: function(xhr, status, message) {
+      passwordIncorrect();
+    }
+  });
+
+  return false;
+}
+
+$("#password-form").submit(login);
