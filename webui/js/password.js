70af8d0a4aea580dfd0cc0798708b74c9c887701
Tony Arcieri <bascule@gmail.com>
2014-05-05 01:01:10 -0700

    CSS fixups

--- webui/js/password.js
+++ webui/js/password.js
@@ -1,6 +1,8 @@
 "use strict";
 
 function openSesame() {
+  $("#door-l").addClass("door-moving");
+  $("#door-r").addClass("door-moving");
   $("#door-l").css("transform", "translateX(-150%)");
   $("#door-r").css("transform", "translateX(150%)");
 }
