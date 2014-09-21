62161ef256576da5bc7d54288125c546d4de69b4
Tony Arcieri <bascule@gmail.com>
2014-09-20 19:03:41 -0500

    Stub out login endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ extern crate static_file;
 extern crate mount;
 
 use std::io::net::ip::Ipv4Addr;
-use iron::Iron;
+use iron::{Iron, Request, Response, IronResult};
+use iron::status;
 use static_file::Static;
 use mount::Mount;
+
+fn login(_req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with(status::Ok, "Seems good bro"))
+}
 
 fn main() {
     let mut mount = Mount::new();
 
+    mount.mount("/login", login);
     mount.mount("/", Static::new(Path::new("webui/")));
 
     Iron::new(mount).listen(Ipv4Addr(127, 0, 0, 1), 3000);
