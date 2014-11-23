544e9a8323812e05f52c61021ad2140f1ee1189e
Tony Arcieri <bascule@gmail.com>
2014-11-22 16:11:35 -0800

    Render setup page as root

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,28 @@ extern crate http;
 use std::io::net::ip::Ipv4Addr;
 use nickel::{
   Nickel,
+  Request,
+  Response,
+  HttpRouter,
   StaticFilesHandler
 };
 
 fn main() {
-  let mut server = Nickel::new();
+  let addr = Ipv4Addr(127, 0, 0, 1);
   let port = 3000;
 
+  let mut server = Nickel::new();
+  let mut router = Nickel::router();
+
+  router.get("/", root_handler);
+
+  server.utilize(router);
   server.utilize(StaticFilesHandler::new("frontend/"));
 
-  println!("Listening on http://localhost:{}", port);
-  server.listen(Ipv4Addr(127, 0, 0, 1), port);
+  println!("Listening on http://{}:{}", addr, port);
+  server.listen(addr, port);
+}
+
+fn root_handler(_request: &Request, response: &mut Response) {
+  response.send_file(&Path::new("frontend/setup.html")).ok();
 }
