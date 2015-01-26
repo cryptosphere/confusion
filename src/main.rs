31ee692a382e299976b82cc0ea0cffc9c3839b44
Tony Arcieri <bascule@gmail.com>
2015-01-25 17:06:02 -0800

    Updates for Rust 1.0.0-alpha

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
 extern crate nickel;
-extern crate http;
 
+use nickel::{Nickel, Request, Response, HttpRouter, StaticFilesHandler};
 use std::io::net::ip::Ipv4Addr;
-use nickel::{
-  Nickel,
-  Request,
-  Response,
-  HttpRouter,
-  StaticFilesHandler
-};
-
-mod encrypted_store;
 
 fn main() {
   let addr = Ipv4Addr(127, 0, 0, 1);
   let port = 3000;
 
   let mut server = Nickel::new();
-  let mut router = Nickel::router();
 
-  router.get("/", root_handler);
+  fn root_handler(_request: &Request, response: &mut Response) {
+    response.send_file(&Path::new("frontend/setup.html")).ok();
+  }
 
-  server.utilize(router);
+  let root: fn(&Request, &mut Response) = root_handler;
+
+  server.get("/", root);
   server.utilize(StaticFilesHandler::new("frontend/"));
-
-  println!("Listening on http://{}:{}", addr, port);
   server.listen(addr, port);
-}
-
-fn root_handler(_request: &Request, response: &mut Response) {
-  response.send_file(&Path::new("frontend/setup.html")).ok();
 }
