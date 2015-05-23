7acaec6072bf76c4b4abaf3762513bc2997ef3b8
Tony Arcieri <bascule@gmail.com>
2015-05-23 11:11:07 -0700

    Rust 1.0 compatibility updates

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-extern crate nickel;
+#[macro_use] extern crate nickel;
 
-use nickel::{Nickel, Request, Response, HttpRouter, StaticFilesHandler};
-use std::io::net::ip::Ipv4Addr;
+use nickel::{Nickel, HttpRouter, StaticFilesHandler};
+use std::path::Path;
 
 fn main() {
-  let addr = Ipv4Addr(127, 0, 0, 1);
-  let port = 3000;
-
   let mut server = Nickel::new();
 
-  fn root_handler(_request: &Request, response: &mut Response) {
-    response.send_file(&Path::new("frontend/setup.html")).ok();
-  }
-
-  let root: fn(&Request, &mut Response) = root_handler;
-
-  server.get("/", root);
+  server.get("/", middleware! { |_request, response|
+    return response.send_file(&Path::new("frontend/setup.html"))
+  });
   server.utilize(StaticFilesHandler::new("frontend/"));
-  server.listen(addr, port);
+  server.listen("127.0.0.1:3000")
 }
